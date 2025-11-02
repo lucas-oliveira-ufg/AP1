4f42ace35a6c8cc86f64d14a8190ee8f82e8d150
lucas-oliveira-ufg <lucas.rodrigues.ufg@gmail.com>
2025-11-02 11:04:34 -0400

    projeto finalizado

--- src/BibFile/BibFile.java
+++ src/BibFile/BibFile.java
@@ -1,6 +1,13 @@
 package BibFile;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class BibFile {
 	
@@ -24,6 +31,100 @@ public class BibFile {
 		}
 		
 		System.out.println("");
+		
+	}
+	
+	public void mkdirCommand(String path) {
+		
+		File directory = new File(path);
+		if (!directory.exists()){
+		    directory.mkdirs();
+		    System.out.println("Diretório criado com sucesso.");
+		} else
+			System.out.println("Diretório já existe. Crie outro diferente.");
+		
+	}
+	
+	public void mvCommand(String source, String target) {
+		
+        Path sourcePath = Paths.get(source);
+        Path targetPath = Paths.get(target); 
+
+        try {
+            Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Arquivo movido com sucesso de " + sourcePath + " para " + targetPath);
+        } catch (IOException e) {
+            System.err.println("Houve um erro ao mover o arquivo: " + e.getMessage());
+        }
+		
+	}
+	
+	public void cpCommand(String source, String target) {
+		
+        Path sourcePath = Paths.get(source); // Path to the source file
+        Path targetPath = Paths.get(target); // Path to the target file
+
+        try {
+            // Create a dummy source file for demonstration
+            //Files.writeString(sourcePath, "This is the content of the source file.");
+
+            // Copy the file, replacing the target if it already exists
+            Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Arquivo copiado com sucesso de " + sourcePath + " para " + targetPath);
+
+        } catch (IOException e) {
+            System.err.println("Houve um erro ao copiar o arquivo: " + e.getMessage());
+        }
+		
+	}
+	
+	public void rmCommand(String path) {
+		
+        Path filePath = Paths.get(path); 
+
+        try {
+            if (Files.deleteIfExists(filePath)) {
+                System.out.println("Arquivo deletado com sucesso.");
+            } else {
+                System.out.println("Arquivo não existe ou não pode ser deletado.");
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao deletar arquivo: " + e.getMessage());
+        }
+		
+	}
+	
+	public void touchCommand(String filePath) {
+		
+		File file = new File(filePath);
+		
+		try {
+			if(file.createNewFile())
+				System.out.println("Arquivo criado com sucesso!");
+			else
+				System.out.println("Arquivo já existe ou não pode ser criado!");
+		} catch(IOException e) {
+			System.out.println("Um erro ocorreu: " + e.getMessage());
+		}
+		
+		
+	}
+	
+	public void lessCommand(String filePath) {
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(filePath));) {
+			
+			String linha;
+			while((linha = br.readLine()) != null) {
+				
+				System.out.println(linha);
+				
+			}
+			
+			
+		} catch(IOException e) {
+			System.out.println("Um erro ocorreu: " + e.getMessage());
+		}
 		
 	}
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -17,38 +17,63 @@ public class Main {
 			switch (choice) {
             
             case 1:
-            	System.out.print("Digite o caminho da pasta\nExemplo: /home/lucasrod/Documentos/viagem\n");
+            	System.out.print("Digite o caminho da pasta\nExemplo: /home/lucasrod/Downloads\n");
             	String path = scan.nextLine();
             	String caminho = scan.nextLine();
             	fileHandling.lsCommand(caminho);
             	break;
             	
             case 2:
-            	System.out.println("\nVocê escolheu Mostrar Materiais\n");
+            	System.out.println("Digite o caminho e nome do novo diretório\nExemplo: /home/lucasrod/<nome_novo_diretorio>");
+            	path = scan.nextLine();
+            	caminho = scan.nextLine();
+            	fileHandling.mkdirCommand(caminho);
             	break;
             	
             case 3:
-            	System.out.println("\nVocê escolheu Cadastrar Cliente\n");            	
+            	System.out.println("Digite o caminho e o nome do arquivo a ser movido\nExemplo: /home/lucasrod/<arquivo_a_ser_movido.txt>");
+            	path = scan.nextLine();
+            	String toMove = scan.nextLine();
+            	System.out.println("");
+            	System.out.println("Digite o caminho de destino e o nome do arquivo\nExemplo: /home/lucasrod/Documentos/<arquivo_a_ser_movido.txt>");
+            	String target = scan.nextLine();
+            	fileHandling.mvCommand(toMove, target);            	
             	break;
             
             case 4:
-            	System.out.println("\nVocê escolheu Cadastrar Cliente VIP\n");
+            	System.out.println("Digite o caminho e o nome do arquivo a ser copiado\nExemplo: /home/lucasrod/<arquivo_a_ser_copiado.txt>");
+            	path = scan.nextLine();
+            	toMove = scan.nextLine();
+            	System.out.println("");
+            	System.out.println("Digite o caminho de destino e o nome do novo arquivo\nExemplo: /home/lucasrod/Documentos/<nova_copia.txt>");
+            	target = scan.nextLine();
+            	fileHandling.cpCommand(toMove, target);
             	break;
             	
             case 5:
-            	System.out.println("\nVocê escolheu Efetuar Venda\n");
+            	System.out.println("Digite o caminho e o nome do arquivo a ser deletado\nExemplo: /home/lucasrod/<arquivo_a_ser_deletado.txt>");
+            	path = scan.nextLine();
+            	String toDelete = scan.nextLine();
+            	fileHandling.rmCommand(toDelete);
             	break;
             	
             case 6:
-            	System.out.println("");
+            	System.out.println("Digite o caminho e o nome do arquivo a ser criado\nExemplo: /home/lucasrod/Documentos/arquivo.txt");
+            	path = scan.nextLine();
+            	String toCreate = scan.nextLine();
+            	fileHandling.touchCommand(toCreate);
             	break;
             	
             case 7:
-            	System.out.println("");
+            	String diretorioAtual = System.getProperty("user.dir");
+            	System.out.println("Diretório atual: " + diretorioAtual);
             	break;
             	
             case 8:
-            	System.out.println("");
+            	System.out.println("Digite o caminho e o nome do arquivo a ser apresentado\nExemplo: /home/lucasrod/Documentos/arquivo.txt");
+            	path = scan.nextLine();
+            	String toShow = scan.nextLine();
+            	fileHandling.lessCommand(toShow);
             	break;
             } 
                         	
@@ -62,17 +87,17 @@ public class Main {
 	}
 	
 	public static void Menu() {
-		System.out.print(  "**************************************************************\n");
-		System.out.println("**** 1  - ls - Listar Conteúdo de um Diretório              ****");
-		System.out.println("**** 2  - cd - Muda o Diretório de Trabalho                 ****");
-		System.out.println("**** 3  - mkdir - Cria Novos Diretórios                     ****");
-		System.out.println("**** 4  - cp - Copia Arquivos ou Diretórios                 ****");
-		System.out.println("**** 5  - mv - Move ou renomeia arquivos ou diretórios      ****");
-		System.out.println("**** 6  - rm - Remove arquivos                              ****");
-		System.out.println("**** 7  - touch - Cria arquivos vazios                      ****");
-		System.out.println("**** 8  - pwd - Exibe o nome do diretório atual             ****");
-		System.out.println("**** 0  - Sair                                              ****");
-		System.out.print(  "****************************************************************");
+		System.out.print(  "*************************************************************************\n");
+		System.out.println("**** 1  - ls - Listar Conteúdo de um Diretório                       ****");
+		System.out.println("**** 2  - mkdir - Cria Novos Diretórios                              ****");
+		System.out.println("**** 3  - mv - Move arquivos ou diretórios                           ****");
+		System.out.println("**** 4  - cp - Copia Arquivos ou Diretórios                          ****");
+		System.out.println("**** 5  - rm - Remove arquivos                                       ****");
+		System.out.println("**** 6  - touch - Cria arquivos vazios                               ****");
+		System.out.println("**** 7  - pwd - Exibe o nome do diretório atual                      ****");
+		System.out.println("**** 8  - less - Apresenta o conteúdo de um arquivo de texto ou log  ****");
+		System.out.println("**** 0  - Sair                                                       ****");
+		System.out.print(  "*************************************************************************");
     	System.out.print("\nDigite uma opção: ");
 	}
 
